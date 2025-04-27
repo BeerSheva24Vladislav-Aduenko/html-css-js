f913f14c5f241d7699a9e8074ce881de6b2d60ce
VladislavAduenko <adyenko.job@gmail.com>
2025-04-27 22:31:45 +0300

    table chess

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-function calculate(el) {
-  const parent = el.parentElement;  
-  const base = parent.querySelector(".number").textContent;
-  const exponent = parent.querySelector(".power").textContent;
-  const result = Math.pow(base, exponent);
-  parent.querySelector(".result").textContent = result;
+const board = document.getElementsByClassName("container")[0];
+
+for (let i = 0; i < 64; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+
+  const row = Math.floor(i / 8);
+  const col = i % 8;
+
+  if ((row + col) % 2 === 0) {
+    cell.classList.add("white");
+  } else {
+    cell.classList.add("black");
+  }
+  board.appendChild(cell);
 }
