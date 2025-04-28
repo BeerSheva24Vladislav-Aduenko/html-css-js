870b7d9c797c6c30cacf822a10a6231b8b609bac
VladislavAduenko <adyenko.job@gmail.com>
2025-04-28 14:52:04 +0300

    chess table by grid

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-const board = document.getElementsByClassName("container")[0];
+// const board = document.getElementsByClassName("container")[0];
 
-for (let i = 0; i < 64; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
+// for (let i = 0; i < 64; i++) {
+//   const cell = document.createElement("div");
+//   cell.classList.add("cell");
 
-  const row = Math.floor(i / 8);
-  const col = i % 8;
+//   const row = Math.floor(i / 8);
+//   const col = i % 8;
 
-  if ((row + col) % 2 === 0) {
-    cell.classList.add("white");
-  } else {
-    cell.classList.add("black");
-  }
-  board.appendChild(cell);
-}
+//   if ((row + col) % 2 === 0) {
+//     cell.classList.add("white");
+//   } else {
+//     cell.classList.add("black");
+//   }
+//   board.appendChild(cell);
+// }
